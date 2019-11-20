47f1b5174c3748b8a61bf42d9ff7e437b7f45045
pjqdyd <1909025079@qq.com>
2019-11-20 21:14:33 +0800

    路由模式

---
+++ springboot-webflux-router-demo/src/main/java/com/pjqdyd/SpringBootWebfluxRouterApplication.java
@@ -0,0 +1,19 @@
+package com.pjqdyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**   
+ * @Description:  [webflux 的路由方式启动类]
+ * @Author:       pjqdyd
+ * @Version:      [v1.0.0]
+ */
+
+@SpringBootApplication
+public class SpringBootWebfluxRouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebfluxRouterApplication.class, args);
+    }
+
+}
---
+++ springboot-webflux-router-demo/src/main/java/com/pjqdyd/routes/TestRouter.java
@@ -0,0 +1,30 @@
+package com.pjqdyd.routes;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.*;
+import reactor.core.publisher.Mono;
+
+/**   
+ * @Description:  [测试路由]
+ * @Author:       pjqdyd
+ * @Version:      [v1.0.0]
+ */
+
+@Configuration
+public class TestRouter {
+
+    /**
+     * 单路由
+     */
+    @Bean
+    public RouterFunction<ServerResponse> routerFunction(){
+        return RouterFunctions.route(
+                RequestPredicates.GET("/sayHiByRoute"), request -> {
+                    Mono<String> result = Mono.just("hello!");
+                    return ServerResponse.ok().body(result, Mono.class);
+                }
+        );
+    }
+
+}
